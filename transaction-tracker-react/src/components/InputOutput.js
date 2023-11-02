495eb9092994c4306a1dcbe68622ec9690bdd6c2
Zihan Wang <ziw224@lehigh.edu>
2023-11-02 15:51:01 -0400

    balance component

--- transaction-tracker-react/src/components/InputOutput.js
+++ transaction-tracker-react/src/components/InputOutput.js
@@ -2,16 +2,16 @@ import React from "react";
 
 export const InputOutput = () => {
   return (
-    <div class="inc-exp-container">
+    <div className ="inc-exp-container">
       <div>
-        <h4>Spent</h4>
-        <p id="money-plus" class="money plus">
+        <h4>Unspent</h4>
+        <p className ="money plus">
           +$0.00
         </p>
       </div>
       <div>
-        <h4>Unspent</h4>
-        <p id="money-minus" class="money minus">
+        <h4>Spent</h4>
+        <p className ="money minus">
           -$0.00
         </p>
       </div>
