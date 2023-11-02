26d455518704f75d05c78f0cc09c6a8f062e9fb9
Zihan Wang <ziw224@lehigh.edu>
2023-11-02 15:49:39 -0400

    adding transactions component

---
+++ transaction-tracker-react/src/components/AddTransaction.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export const AddTransaction = () => {
+
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
