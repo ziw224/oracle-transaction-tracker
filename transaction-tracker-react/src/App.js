22ae6c3e9760c21cfff233869e2cd08d23fd8be7
Zihan Wang <ziw224@lehigh.edu>
2023-11-02 15:51:25 -0400

    update on App.js

--- transaction-tracker-react/src/App.js
+++ transaction-tracker-react/src/App.js
@@ -1,18 +1,23 @@
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { InputOutput } from "./components/InputOutput";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <InputOutput />
+        <TransactionList />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
