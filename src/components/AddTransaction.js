12eecd654b67290bc2189ac93d86fd5edde55b19
Zihan Wang <ziw224@lehigh.edu>
2023-11-21 13:54:30 -0500

    UI revisement

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -2,29 +2,45 @@ import React, { useState, useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
-
   const [text, setText] = useState("");
   const [name, setName] = useState("");
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState();
 
-  const {addTransaction} = useContext(GlobalContext);
-  
-  const onSubmit = e => {
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
     e.preventDefault();
+
+    // Validate input parameters
+    if (!name.trim()) {
+      alert("Wallet Address is required");
+      return;
+    }
+
+    if (!amount || isNaN(amount)) {
+      alert("Amount must be a valid number");
+      return;
+    }
+
     const newTransaction = {
-      id: Math.floor(Math.random()*100000000),
+      id: Math.floor(Math.random() * 100000000),
       text,
       name,
-      amount: +amount // turing "amount" into a number
-    }
-    addTransaction(newTransaction);
-  }
+      amount: -parseFloat(amount), // Convert amount to a negative number
+    };
 
+    addTransaction(newTransaction);
+
+    // Clear input fields after submitting
+    setText("");
+    setName("");
+    setAmount(0);
+  };
 
   return (
     <>
       <h3>Add new transaction</h3>
-      <form onSubmit = {onSubmit}>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
@@ -35,19 +51,16 @@ export const AddTransaction = () => {
           />
         </div>
         <div className="form-control">
-          <label htmlFor="user2">User2</label>
+          <label htmlFor="user2">Wallet Address</label>
           <input
-            type="user2"
+            type="text"
             value={name}
             onChange={(event) => setName(event.target.value)}
-            placeholder="Enter user name..."
+            placeholder="Enter address..."
           />
         </div>
         <div className="form-control">
-          <label htmlFor="amount">
-            Amount <br />
-            (negative - expense, positive - income)
-          </label>
+          <label htmlFor="amount">Amount</label>
           <input
             type="number"
             value={amount}
