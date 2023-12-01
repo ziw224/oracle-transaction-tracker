c2c895cdb2d8aeb02035cfb81580d41c39020973
Zee Khan <zek224@lehigh.edu>
2023-12-01 18:04:58 -0500

    Add fetch button to DataTable component

--- src/pages/admin/list/List.js
+++ src/pages/admin/list/List.js
@@ -2,12 +2,30 @@ import "./list.css";
 import { AdminSidebar } from "../../../components/adminComponents/sidebar/AdminSidebar";
 import { DataTable } from "../../../components/adminComponents/dataTable/DataTable";
 
-export const List = ({ type }) => {       // Accept the 'type' prop here
+export const List = ({ type }) => {
+  const handleButtonClick = async () => {
+    try {
+      const response = await fetch('/command/new-wallet', {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    }
+  };
+
   return (
     <div className="admin-list">
       <AdminSidebar />
       <div className="listContainer">
-        <DataTable type={type} /> {/* Pass the 'type' prop to DataTable */}
+        <div className="dataTableContainer">
+          <DataTable type={type} />
+          <button onClick={handleButtonClick} className="fetchButton">New User</button> {/* Add your button here */}
+        </div>
       </div>
     </div>
   );
