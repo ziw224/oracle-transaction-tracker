62bdcc917b4752b6472bfe9efcb462445df0f6f2
Zee Khan <zek224@lehigh.edu>
2023-12-01 14:45:59 -0500

    Refactor DataTable and Widget components

--- src/components/adminComponents/dataTable/DataTable.js
+++ src/components/adminComponents/dataTable/DataTable.js
@@ -1,4 +1,4 @@
-import "./datatable.css";
+import React, { useState, useEffect } from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,50 +6,51 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
-function createData(
-  hashVal: string,
-  wallet: string,
-  amount: number,
-  date: string,
-) {
-  return { hashVal, wallet, amount, date };
-}
-
-const rows = [
-  createData('0001', 'usd1qrw038lx5n4wxx3yvuwdndpr7gnm347d6pn37uywgudzq90w7fsuk52kd5u', 25, '10/23'),
-];
+import "./datatable.css";
 
 export const DataTable = () => {
+  const [wallets, setWallets] = useState([]);
+
+  useEffect(() => {
+    // Fetch the wallet data from the server
+    const fetchWalletData = async () => {
+      try {
+        const response = await fetch('/cbdc-wallets'); // This is the endpoint to fetch wallets
+        const data = await response.json();
+        setWallets(data.wallets); // Set the wallets state with the fetched data
+      } catch (error) {
+        console.error("Failed to fetch wallet data:", error);
+        // Handle error state here
+      }
+    };
+
+    fetchWalletData();
+  }, []); // Dependency array is empty, so the effect runs once on mount
+
   return (
     <div className="datatable">
-        <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>HASH VALUE</TableCell>
-            <TableCell align="center">WALLET ADDRESS 2</TableCell>
-            <TableCell align="right">TRANSACTION AMOUNT &nbsp;($)</TableCell>
-            <TableCell align="right">TRANSACTION DATE</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.hashVal}
-              </TableCell>
-              <TableCell align="right">{row.wallet}</TableCell>
-              <TableCell align="center">{row.amount}</TableCell>
-              <TableCell align="center">{row.date}</TableCell>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>WALLET NUMBER</TableCell>
+              <TableCell>WALLET ADDRESS</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {wallets.map((wallet) => (
+              <TableRow key={wallet.wallet_number}>
+                <TableCell component="th" scope="row">
+                  {wallet.wallet_number}
+                </TableCell>
+                <TableCell>{wallet.wallet_address}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
+
+export default DataTable;
