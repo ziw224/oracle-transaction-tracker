05507b8bcc30627773df42ed9cf4b3dd99ca2d63
Zee Khan <zek224@lehigh.edu>
2023-12-03 03:41:21 -0500

    Remove unused code for displaying spent value

--- src/components/homeComponents/Balance.js
+++ src/components/homeComponents/Balance.js
@@ -4,7 +4,6 @@ import { GlobalWalletContext } from "../../context/WalletContext";
 export const Balance = () => {
   const { selectedWalletNumber } = useContext(GlobalWalletContext);
   const [balance, setBalance] = useState("0.00");
-  const [spent, setSpent] = useState("0.00"); // If you need to display spent here as well
 
   useEffect(() => {
     const fetchBalance = async () => {
@@ -18,17 +17,10 @@ export const Balance = () => {
           if (balanceMatch && balanceMatch[1]) {
             setBalance(balanceMatch[1]);
           }
-
-          // Fetch the spent value
-          // Replace this URL with your actual endpoint for fetching the spent value
-          const spentResponse = "0.00";
-          setSpent(spentResponse);
         }
       } catch (error) {
         console.error("Failed to fetch wallet data:", error);
         setBalance("0.00");
-        // Handle error for spent value as well
-        setSpent("0.00");
       }
     };
 
@@ -39,9 +31,6 @@ export const Balance = () => {
     <>
       <h4>Your Balance</h4>
       <h1>${balance}</h1>
-      {/* Display spent value if necessary */}
-      <h4>Spent</h4>
-      <h1>${spent}</h1>
     </>
   );
 };
