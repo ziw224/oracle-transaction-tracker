db5607359f01e9dda36e594b4efe8ce6c72bb79f
Zee Khan <zek224@lehigh.edu>
2023-12-03 01:52:18 -0500

    Add wallet data to dropdown menu

--- src/components/homeComponents/navbar/DropdownMenu.js
+++ src/components/homeComponents/navbar/DropdownMenu.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FaceIcon from '@mui/icons-material/Face';
-
 import "./navbar.css";
 
-
 export const DropdownMenu = () => {
+  const [wallets, setWallets] = useState([]);
+
+  useEffect(() => {
+    const fetchWalletData = async () => {
+      try {
+        const response = await fetch("/cbdc-wallets");
+        const data = await response.json();
+        if (data.wallets && data.wallets.length > 0) {
+          setWallets(data.wallets);
+        } else {
+          setWallets([]); // Set to empty array if there are no wallets
+        }
+      } catch (error) {
+        console.error("Failed to fetch wallet data:", error);
+        setWallets([]); // Set to empty array in case of an error
+      }
+    };
+
+    fetchWalletData();
+  }, []);
+  
   function DropdownItem(props) {
     return (
       <a className="menu-item">
@@ -13,11 +32,14 @@ export const DropdownMenu = () => {
       </a>
     );
   }
+
   return (
     <div className="dropdown">
-      <DropdownItem leftIcon={<FaceIcon/>}>user1</DropdownItem>
-      <DropdownItem leftIcon={<FaceIcon/>}>user2</DropdownItem>
-      <DropdownItem leftIcon={<FaceIcon/>}>user3</DropdownItem>
+      {wallets.map((wallet, index) => (
+        <DropdownItem leftIcon={<FaceIcon />} key={wallet.wallet_number}>
+          User {wallet.wallet_number}
+        </DropdownItem>
+      ))}
     </div>
   );
 };
