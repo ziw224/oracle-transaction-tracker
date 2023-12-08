21309db93fe0e380395e9c2fcfad80d0bbf7d9f1
Zee Khan <zek224@lehigh.edu>
2023-12-08 12:33:27 -0500

    hotfix

--- src/components/homeComponents/AddTransaction.js
+++ src/components/homeComponents/AddTransaction.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 import { GlobalWalletContext } from "../../context/WalletContext";
 
